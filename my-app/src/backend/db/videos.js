c934509030e939b0cec574cab0864fff70da0e05
akshatgupta105 <53713926+Dev-AkshatGupta@users.noreply.github.com>
2022-03-28 09:01:55 +0530

    Data Been created and some modification in jsx

--- my-app/src/backend/db/videos.js
+++ my-app/src/backend/db/videos.js
@@ -4,18 +4,414 @@
  * */
 
 export const videos = [
-  {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+{
+    _id: uuid(),
+    categoryName: "American Classics",
+    creator: "MotorTrend Channel",
+    creatorLogo: {
+      altText: "MotorTrend Channel Logo",
+      url: "https://yt3.ggpht.com/ytc/AKedOLToqTpU28e86K3L2vF1M3siUKh2GIWxGf2JQ2Hvzw=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "64 Chevrolet Impala SS by Jimmy Humilde! | LOWRIDER Roll Models Season 5 Ep. 12 ",
+      url: "https://i.ytimg.com/vi/E8GzE_pMsWU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBQ5y_-6dtH2zmz5ioNpmdD8UEhnA",
+    },
+    title: "64 Chevrolet Impala SS by Jimmy Humilde! | LOWRIDER Roll Models Season 5 Ep. 12 ",
+    
   },
-  {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+{
+    _id: uuid(),
+    categoryName: "American Classics",
+    creator: "MotorTrend Channel",
+    creatorLogo: {
+      altText: "MotorTrend Channel Logo",
+      url: "https://yt3.ggpht.com/ytc/AKedOLToqTpU28e86K3L2vF1M3siUKh2GIWxGf2JQ2Hvzw=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "62 Chevrolet Impala SS! | LOWRIDER Roll Models Season 5 Episode 11s",
+      url: "https://i.ytimg.com/vi/8HP5COGRfC0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCRHw-Qq_Ill9d8a3fzs2ej0APJdg",
+    },
+    title: "62 Chevrolet Impala SS! | LOWRIDER Roll Models Season 5 Episode 11",
+    
   },
-];
+{
+    _id: uuid(),
+    categoryName: "Bikes",
+    creator: "History",
+    creatorLogo: {
+      altText: "History Logo",
+      url: "https://yt3.ggpht.com/IVVrue_WHD9lK-12DvplMxKoxOYOS2kjYl1tbhC_fONBK06E-YfdEdtScQNwWQUbxrHfwnxBgw=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "Counting Cars: Ryan's SECRET Project is ICONIC Chopper History (Season 4) | History",
+      url: "https://i.ytimg.com/vi/pq8nKiS2Zxg/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCnrKsxE446seiZ_dy3hFkNuI5EXg",
+    },
+    title: "Counting Cars: Ryan's SECRET Project is ICONIC Chopper History (Season 4) | History",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Bikes",
+    creator: "History",
+    creatorLogo: {
+      altText: "History Logo",
+      url: "https://yt3.ggpht.com/IVVrue_WHD9lK-12DvplMxKoxOYOS2kjYl1tbhC_fONBK06E-YfdEdtScQNwWQUbxrHfwnxBgw=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "Counting Cars: The Les Paul Project | History",
+      url: "https://i.ytimg.com/vi/kLSSBYyoL6w/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBv0eTmDgLeiqFxvrplKdqSSDTAyg",
+    },
+    title: "Counting Cars: The Les Paul Project | History",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Bikes",
+    creator: "History",
+    creatorLogo: {
+      altText: "History Logo",
+      url: "https://yt3.ggpht.com/IVVrue_WHD9lK-12DvplMxKoxOYOS2kjYl1tbhC_fONBK06E-YfdEdtScQNwWQUbxrHfwnxBgw=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "Counting Cars: Danny Reveals a Crazy New Paint Job for a Motorcycle (S7, E16) | History",
+      url: "https://i.ytimg.com/vi/E3nfUuQWyRM/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA1-kf4s4D4-J1sQuruiPjzM2MUFw",
+    },
+    title: "Counting Cars: Danny Reveals a Crazy New Paint Job for a Motorcycle (S7, E16) | History",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Bikes",
+    creator: "History",
+    creatorLogo: {
+      altText: "History Logo",
+      url: "https://yt3.ggpht.com/IVVrue_WHD9lK-12DvplMxKoxOYOS2kjYl1tbhC_fONBK06E-YfdEdtScQNwWQUbxrHfwnxBgw=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "Counting Cars: Les Paul Motorcycle | History",
+      url: "https://i.ytimg.com/vi/R5XMKUOm0pk/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBXlU7ufnUvIbzOFOrujFcJjgFUNw",
+    },
+    title: "Counting Cars: Les Paul Motorcycle | History",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Bikes",
+    creator: "History",
+    creatorLogo: {
+      altText: "History Logo",
+      url: "https://yt3.ggpht.com/IVVrue_WHD9lK-12DvplMxKoxOYOS2kjYl1tbhC_fONBK06E-YfdEdtScQNwWQUbxrHfwnxBgw=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "Counting Cars: Dee Snider ROCKS OUT on TWISTED Custom Chopper (Season 6) | History",
+      url: "https://i.ytimg.com/vi/R_jY1InrTys/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAA5_-aIciVfTtyDaFkUDgxFKqo3Q",
+    },
+    title: "Counting Cars: Dee Snider ROCKS OUT on TWISTED Custom Chopper (Season 6) | History",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "American Classics",
+    creator: "MotorTrend Channel",
+    creatorLogo: {
+      altText: "MotorTrend Channel Logo",
+      url: "https://yt3.ggpht.com/ytc/AKedOLToqTpU28e86K3L2vF1M3siUKh2GIWxGf2JQ2Hvzw=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "Rudy Campos & His 1946 Chevrolet Fleetmaster - Lowrider Roll Models Ep. 6",
+      url: "https://i.ytimg.com/vi/_tMzf2L-osg/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDdjqQ2f6FH6OaT-y4bDer8QNbEwA",
+    },
+    title: "Rudy Campos & His 1946 Chevrolet Fleetmaster - Lowrider Roll Models Ep. 6",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Car Reviews",
+    creator: "Fuel Injected",
+    creatorLogo: {
+      altText: "Fuel Injected Logo",
+      url: "https://yt3.ggpht.com/ytc/AKedOLQHjeIxLhS99NEyNHzoZz5-C1L_p5CSp7sj9d1p3A=s48-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "Two Noobs and a Modified Royal Enfield : Holy Shift : Episode 1 : PowerDrift",
+      url: "https://i.ytimg.com/vi/6oDndmNFO5I/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBllLPgHJpk_DuXkqTsqlHXZCy0PQ",
+    },
+    title: "Skoda Octavia 2021 Real Life Review - 2.0 TSI Is Superbbbbb !!!!!",
+    
+  },
+
+ {
+    _id: uuid(),
+    categoryName: "Car Reviews",
+    creator: "Faisal Khan",
+    creatorLogo: {
+      altText: "Faisal Khan Logo",
+      url: "https://yt3.ggpht.com/BF1snvaP4mCXDuBvx88j5fdLpcAoEKYbMbTUs6jX23cI7eu2_OtvNzf_AxpHkOgGKU0b4-5dyiI=s88-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "Lexus NX 350h - Surprisingly Fun & Quite Efficient | Faisal Khan",
+      url: "https://i.ytimg.com/vi/IRwEYAq9SwE/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA1V0T8nJpTcd0frP2eIGz_m9haqg",
+    },
+    title: "Lexus NX 350h - Surprisingly Fun & Quite Efficient | Faisal Khan",
+    
+  },
+
+
+ {
+    _id: uuid(),
+    categoryName: "Car Reviews",
+    creator: "Faisal Khan",
+    creatorLogo: {
+      altText: "Faisal Khan Logo",
+      url: "https://yt3.ggpht.com/BF1snvaP4mCXDuBvx88j5fdLpcAoEKYbMbTUs6jX23cI7eu2_OtvNzf_AxpHkOgGKU0b4-5dyiI=s88-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "Audi RS5 Sportback - Amazing Acceleration, Sound & Grip | Faisal Khan",
+      url: "https://i.ytimg.com/vi/rXdxTdNRGic/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBHI3XRUE8vuf1U7NKu7_RaTFyqmw",
+    },
+    title: "Audi RS5 Sportback - Amazing Acceleration, Sound & Grip | Faisal Khan",
+    
+  },
+
+  {
+    _id: uuid(),
+    categoryName: "Car Reviews",
+    creator: "Faisal Khan",
+    creatorLogo: {
+      altText: "Faisal Khan Logo",
+      url: "https://yt3.ggpht.com/BF1snvaP4mCXDuBvx88j5fdLpcAoEKYbMbTUs6jX23cI7eu2_OtvNzf_AxpHkOgGKU0b4-5dyiI=s88-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "Volkswagen Virtus GT - Practical Sedan With Sporty Looks! | Faisal Khan",
+      url: "https://i.ytimg.com/vi/Yzs4g8vqs4M/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDBp1kd6yc3yMp9m4xA7iRRZ4tD6Q",
+    },
+    title: "Volkswagen Virtus GT - Practical Sedan With Sporty Looks! | Faisal Khan",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Car Reviews",
+    creator: "Faisal Khan",
+    creatorLogo: {
+      altText: "Faisal Khan Logo",
+      url: "https://yt3.ggpht.com/BF1snvaP4mCXDuBvx88j5fdLpcAoEKYbMbTUs6jX23cI7eu2_OtvNzf_AxpHkOgGKU0b4-5dyiI=s88-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "2022 Maruti Celerio - Fantastic Engine But Overpriced | Faisal Khan",
+      url: "https://i.ytimg.com/vi/G2KK-x0O0cY/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBzZBSJpDZSj4e0bh-P-yOzLiIYFg",
+    },
+    title: "2022 Maruti Celerio - Fantastic Engine But Overpriced | Faisal Khan",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Custom",
+    creator: "PowerDrift",
+    creatorLogo: {
+      altText: "PowerDrift Logo",
+      url: "https://yt3.ggpht.com/ytc/AKedOLS3H1iDFct-st71_rCt-dWiUPYkt5PCTsI_B92T=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "Two Noobs and a Modified Royal Enfield : Holy Shift : Episode 1 : PowerDrift",
+      url: "https://i.ytimg.com/an_webp/NlHG98RGles/mqdefault_6s.webp?du=3000&sqp=CJu3gpIG&rs=AOn4CLBTIlFpT2G0hZ6kj9H3qyg2_0uYGQ",
+    },
+    title: "Two Noobs and a Modified Royal Enfield : Holy Shift : Episode 1 : PowerDrift",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Custom",
+    creator: "PowerDrift",
+    creatorLogo: {
+      altText: "PowerDrift Logo",
+      url: "https://yt3.ggpht.com/ytc/AKedOLS3H1iDFct-st71_rCt-dWiUPYkt5PCTsI_B92T=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "A smurfed out Pulsar and a chopped KTM 390 Duke : Holy Shift : Episode 3 : PowerDrift",
+      url: "https://i.ytimg.com/an_webp/Ui3OX8Zyh0s/mqdefault_6s.webp?du=3000&sqp=CK2bgpIG&rs=AOn4CLB7R9m4M8RzP8dBxLd3FZiuFYFtXw",
+    },
+    title: "A smurfed out Pulsar and a chopped KTM 390 Duke : Holy Shift : Episode 3 : PowerDrift",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Custom",
+    creator: "PowerDrift",
+    creatorLogo: {
+      altText: "PowerDrift Logo",
+      url: "https://yt3.ggpht.com/ytc/AKedOLS3H1iDFct-st71_rCt-dWiUPYkt5PCTsI_B92T=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "A smurfed out Pulsar and a chopped KTM 390 Duke : Holy Shift : Episode 3 : PowerDrift",
+      url: "https://i.ytimg.com/an_webp/Ui3OX8Zyh0s/mqdefault_6s.webp?du=3000&sqp=CK2bgpIG&rs=AOn4CLB7R9m4M8RzP8dBxLd3FZiuFYFtXw",
+    },
+    title: "A smurfed out Pulsar and a chopped KTM 390 Duke : Holy Shift : Episode 3 : PowerDrift",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Custom",
+    creator: "PowerDrift",
+    creatorLogo: {
+      altText: "PowerDrift Logo",
+      url: "https://yt3.ggpht.com/ytc/AKedOLS3H1iDFct-st71_rCt-dWiUPYkt5PCTsI_B92T=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "KTM 390 Duke meets Star Wars : Holy Shift : Episode 4 : PowerDrift",
+      url: "https://i.ytimg.com/vi/GmaRqbqI-8M/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDoRz-D_8qrN3LXCj8ni-CODd48VA",
+    },
+    title: "KTM 390 Duke meets Star Wars : Holy Shift : Episode 4 : PowerDrift",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Custom",
+    creator: "PowerDrift",
+    creatorLogo: {
+      altText: "PowerDrift Logo",
+      url: "https://yt3.ggpht.com/ytc/AKedOLS3H1iDFct-st71_rCt-dWiUPYkt5PCTsI_B92T=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "A smurfed out Pulsar and a chopped KTM 390 Duke : Holy Shift : Episode 3 : PowerDrift",
+      url: "https://i.ytimg.com/vi/Ui3OX8Zyh0s/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBNPlf0nW2PCWbcdwSi8CKVX7bOEQ",
+    },
+    title: "A smurfed out Pulsar and a chopped KTM 390 Duke : Holy Shift : Episode 3 : PowerDrift",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Custom",
+    creator: "PowerDrift",
+    creatorLogo: {
+      altText: "PowerDrift Logo",
+      url: "https://yt3.ggpht.com/ytc/AKedOLS3H1iDFct-st71_rCt-dWiUPYkt5PCTsI_B92T=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "A van with a plan : Holy Shift : Episode 7 : PowerDrift",
+      url: "https://i.ytimg.com/vi/RL13JPhDiII/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBnu7Rczd-rU6KUJd27tglKwOIZcw",
+    },
+    title: "A van with a plan : Holy Shift : Episode 7 : PowerDrift",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Repairs",
+    creator: "DDS",
+    creatorLogo: {
+      altText: "DDS",
+      url: "https://yt3.ggpht.com/2oKoUK0upImxNllk_vZkrdbYUmdQmQ1VCXsyN8B6S7QPmd6y4os4R3gKBa_1rhIz6cIYvyF43Q=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "पैसे बचा लिए - घर पर Full Service करके।",
+      url: "https://i.ytimg.com/vi/nzkPpIdEbvo/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCqnZ0swz4SXOUlDoYMxsaX20ri1A",
+    },
+    title: "पैसे बचा लिए - घर पर Full Service करके।",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Repairs",
+    creator: "DDS",
+    creatorLogo: {
+      altText: "DDS",
+      url: "https://yt3.ggpht.com/2oKoUK0upImxNllk_vZkrdbYUmdQmQ1VCXsyN8B6S7QPmd6y4os4R3gKBa_1rhIz6cIYvyF43Q=s68-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "पैसे बचा लिए - घर पर Full Service करके।",
+      url: "https://i.ytimg.com/vi/nzkPpIdEbvo/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCqnZ0swz4SXOUlDoYMxsaX20ri1A",
+    },
+    title: "पैसे बचा लिए - घर पर Full Service करके।",
+    
+  },
+{
+    _id: uuid(),
+    categoryName: "Cars Evolution",
+    creator: "Donut Media",
+    creatorLogo: {
+      altText: "Donut Media Logo",
+      url: "https://yt3.ggpht.com/ytc/AKedOLSE8OsDBz1krAyAQpGaZFg-pbF7Tr24IJq_HokKOA=s48-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "Evolution of the Porsche 911 | Donut Media",
+      url: "https://i.ytimg.com/vi/Pz8IGLgFE2s/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAGbXxcuViQOSqkCwcX20cbF9xKFA",
+    },
+    title: " Evolution of the Porsche 911 | Donut Media",
+  },
+{
+    _id: uuid(),
+    categoryName: "Cars Evolution",
+    creator: "Donut Media",
+    creatorLogo: {
+      altText: "Donut Media Logo",
+      url: "https://yt3.ggpht.com/ytc/AKedOLSE8OsDBz1krAyAQpGaZFg-pbF7Tr24IJq_HokKOA=s48-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "Evolution of the Honda Civic Hatch | Donut Media",
+      url: "https://i.ytimg.com/vi/11nLdduJzzI/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCI-FlGdaX2mP8m55ovE1-sH72gEA",
+    },
+    title: "Evolution of the Honda Civic Hatch | Donut Media",
+  },
+{
+    _id: uuid(),
+    categoryName: "Cars Evolution",
+    creator: "Donut Media",
+    creatorLogo: {
+      altText: "Donut Media Logo",
+      url: "https://yt3.ggpht.com/ytc/AKedOLSE8OsDBz1krAyAQpGaZFg-pbF7Tr24IJq_HokKOA=s48-c-k-c0x00ffffff-no-rj",
+    },
+    isInWatchLater: false,
+    isLiked: false,
+    thumbnail: {
+      altText: "Evolution of the Ford Mustang | Donut Media",
+      url: "https://i.ytimg.com/vi/nR3OdeqiEF8/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDEHAeqmYvrYIBe8iInKiuoKiFzew",
+    },
+    title: "Evolution of the Ford Mustang | Donut Media",
+  },
+
+]
